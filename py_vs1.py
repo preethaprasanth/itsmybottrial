464e3a4cab7396480bde5af18c2da5802131b823
preethaprasanth <preethaprasanth@gmail.com>
2024-02-28 17:14:29 +0530

    commit at 13.11 on 28-02-2014 commit at 1.15PM on 28-02-2024 commit at 17.14 on 28-02-2024

--- py_vs1.py
+++ py_vs1.py
@@ -3,4 +3,5 @@
 
 print("hello welcome to Pycharm")
 print("Pycharm GIT HUB Test1-1.10PM Feb28th 2024")
-print("checking conflicst- from Pycharm 1.15PM on Feb 28tyh")
+print("checking conflicts- from Pycharm 1.15PM on Feb 28tyh")
+print("In py_vs1 ->Edit-at 16:56 on 28-02-2024")
--- welcome_1.py
+++ welcome_1.py
@@ -2,3 +2,5 @@
 print("Welcome , GIT BUB-Pycharm Message1 at 28th Feb 2024 11.45AM!")
 
 print("Welcome , GIT BUB-Pycharm Message1 at 28th Feb 2024 1.10PM")
+
+print("Edot in Welcome_1.py at 16:57 on 28-02-2024")
