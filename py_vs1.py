3dcec2b1eb29b964027cc0629b59b898612b68b3
preethaprasanth <preethaprasanth@gmail.com>
2024-02-28 13:11:37 +0530

    commit at 13.11 on 28-02-2014

--- py_vs1.py
+++ py_vs1.py
@@ -2,3 +2,4 @@
 #First Python pycharm
 
 print("hello welcome to Pycharm")
+print("Pycharm GIT HUB Test1-1.10PM Feb28th 2024")
--- welcome_1.py
+++ welcome_1.py
@@ -1,2 +1,4 @@
 #welcome
 print("Welcome , GIT BUB-Pycharm Message1 at 28th Feb 2024 11.45AM!")
+
+print("Welcome , GIT BUB-Pycharm Message1 at 28th Feb 2024 1.10PM")
